f8f50cfb4678f7d54b793375859bc0dcdba98318
Luis Henrique <junimhs10@gmail.com>
2019-02-21 11:35:46 -0300

    Sistema de upload aws

---
+++ README.md
@@ -0,0 +1,12 @@
+Projeto de upload para amazon aws
+No projeto foir criado com.
+-- nodejs
+-- express
+-- multer
+-- multer-s3
+-- aws-sdk
+-- mongodb
+
+Para testar o projeto faça o git clone, crie um arquivo .env, no arquive terá um .env_exemple, apos criar de um npm install e depois um npm run dev
+
+Projeto criado por Luis Henrique - email: junimhs10@gmail.com
---
+++ src/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(require("./routes"));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
+app.listen(3000);
